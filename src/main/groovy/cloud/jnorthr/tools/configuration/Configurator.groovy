f5d8756b397d65b6ed3e79ebc74c27d4dc65ea15
jnorthr <james.b.northrop@googlemail.com>
2016-09-26 23:19:42 +0200

    refactor tests moving them from main() to spocktest

--- src/main/groovy/cloud/jnorthr/tools/configuration/Configurator.groovy
+++ src/main/groovy/cloud/jnorthr/tools/configuration/Configurator.groovy
@@ -6,9 +6,10 @@ import static groovy.json.JsonParserType.*
  * Features to consume application configuration variables
  */
 public class Configurator{
+
 	// Holds text found in the external configuration file
 	String configText="";
-	
+		
 	// The name of the environment within the configuration file that governs following access
 	String environ="";
 	
@@ -19,9 +20,12 @@ public class Configurator{
 	String fn = "${path}/sample.config";
 	
 	/*
-	 * A handle to the configSlurper object holding the text from the configuration file
+	 * A handle to the configSlurper object holding the text from the configuration file.
+	 *
+	 * Holds consumed JSON configuration payload from text found in the external configuration file
 	 */
-	def configHandle
+	ConfigObject configObject;
+	
 	
 	/*
 	 * default constructor mounts the configuration file, if it exist, pointing to the 'prod' environment
@@ -33,7 +37,7 @@ public class Configurator{
 		// Configurator script for Configurator.groovy
 		// see: http://mrhaki.blogspot.fr/2009/10/groovy-goodness-using-configslurper.html
 		configText = new File(fn).text
-		configHandle = createConfig('prod').parse(configText)
+		setup('prod');
 	} // end of default constructor
 	
 	
@@ -49,48 +53,120 @@ public class Configurator{
 		// Configurator script for Configurator.groovy
 		// see: http://mrhaki.blogspot.fr/2009/10/groovy-goodness-using-configslurper.html
 		configText = new File(fn).text
-		configHandle = createConfig('prod').parse(configText)
-	} // end of default constructor
+		setup('prod');
+	} // end of non-default constructor
 	
 	
+	
+	// construct specific runtime environment
+	public boolean setup(String e)
+	{		
+		// Create Configurator slurper and set environment to prod.
+		configObject = createConfig(e).parse(configText)
+	} // end of runner
+
+
+	// internal method to show content of configObject 	
+	def dump() { return "configObject="+configObject.toString(); }
+
+
+	// convenience method	
 	def say(txt) { println txt; }
 
-	// Helper closure to create a new Configurator.groovy for the given environment and
-	// register servers as section with Configurator per environment returning new Configurator object.
+	/*
+	 * Helper closure to create a new ConfigSlurper for the given environment and
+	 * register servers as section with Configurator per environment.
+	 */
 	def createConfig = { env ->
 		say "createConfig for ${env}"
 		environ = env;
-    		def configSlurper = new ConfigSlurper(env)
-    		configSlurper.registerConditionalBlock('servers', env)
-    		configSlurper
+    	def configSlurper = new ConfigSlurper(env)
+    	configSlurper.registerConditionalBlock('servers', env)
+    	return configSlurper  //object
 	} // end of createConfig
 
-	//
-	public boolean runner(String e)
+
+	public  String get(String val)
 	{
-		say "runner environment="+e;
-		environ = e;
-		// Create Configurator slurper and set environment to prod.
-		configHandle = createConfig(e).parse(configText)
+		return configObject.get(val);
+	} // end of method
+	
+	// take Map and include that in the ConfigObject	
+	public boolean overwrite(def m)
+	{
+		configObject.putAll(m)
+	} // end of method
 
-		say "prod Configurator.mail.host="+configHandle.mail.host
-		assert configHandle.mail.host == 'mail.server'
+	// take key/value and include that in the ConfigObject	
+	public boolean put(def k, def v)
+	{
+		configObject.put(k,v)
+	} // end of method
+	
+	// take key and see if it's in our ConfigObject	
+	public boolean has(def k)
+	{
+		configObject.containsKey(k)
+	} // end of method
+		
+	public static void main(String[] args)
+	{
+		println "HELLO from Configurator.groovy"
+		args.each{e-> println "arg="+e}
+		Configurator c;
+		if (args.size() > 0) { c = new Configurator(args[0]) } else { c = new Configurator(); }
+		
+		c.say "Goodbye from Configurator.groovy"
+		c.say "--- the end ---"
+	} // end of main 
 
-		say "prod configHandle.appName="+configHandle.appName
-		return (configHandle.appName == 'production')?true:false;
-	} // end of runner
+} // end of class
+
+/*
+>>
+>> import groovy.json.*
+>>
+>> def options = JsonOutput.options()
+>>         .excludeNulls()
+>>         .excludeFieldsByName('make', 'country', 'record')
+>>         .excludeFieldsByType(Number)
+>>         .addConverter(URL) { url -> '"http://groovy-lang.org"' }
+>>
+>> StringWriter writer = new StringWriter()
+>> StreamingJsonBuilder builder = new StreamingJsonBuilder(writer, options)
+>>
+>> builder.records {
+>>     car {
+>>         name 'HSV Maloo'
+>>         make 'Holden'
+>>         year 2006
+>>         country 'Australia'
+>>         homepage new URL('http://example.org')
+>>         record {
+>>             type 'speed'
+>>             description 'production pickup truck with speed of 271kph'
+>>         }
+>>     }
+>> }
+>>
+>> assert writer.toString() == '{"records":{"car":{"name":"HSV
+>> Maloo","homepage":"http://groovy-lang.org"}}}'
+
 
 
 	public boolean tester1(def e)
 	{
 		say "tester1 --->"+e;
 		// Create Configurator slurper and set environment to local using text in config var.
-		configHandle = createConfig(e).parse(configText)
+		configObject = createConfig(e).parse(configText)
 
-		assert configHandle.mail.host == 'greenmail'
-		return (configHandle.appName == 'local')?true:false;
+		assert configObject.mail.host == 'greenmail'
+		return (configObject.appName == 'local')?true:false;
 	} // end of runner
 
+
+
+	// Try JSON parser
 	public  boolean tester2(def env)
 	{
 		say "tester2 --->"+env;
@@ -111,31 +187,4 @@ public class Configurator{
 		return (conf."${env}" == 'production')?true:false;
 	} // end of method
 
-	public  String getVal(String val)
-	{
-		return configHandle."${val}";
-	} // end of method
-	
-	public static void main(String[] args)
-	{
-		println "HELLO from Configurator.groovy"
-		args.each{e-> println "arg="+e}
-		Configurator c;
-		if (args.size() > 0) { c = new Configurator(args[0]) } else { c = new Configurator(); }
-		if (c.runner('prod'))
-		{
-			c.say "Configurator.appName == 'production' ? ="+c.runner('prod');
-		} // end of if
-
-		c.say "getting email="+c.getVal('email');
-		
-		c.say "tester1()="+c.tester1('local');
-		c.say "getting email="+c.getVal('email');
-
-		// read from internal JSon payload - not config file
-		c.say "tester2()="+c.tester2('env');
-		
-		c.say "Goodbye from Configurator.groovy"
-		c.say "--- the end ---"
-	} // end of main 
-} // end of class
+*/
--- src/test/groovy/cloud/jnorthr/tools/configuration/ConfiguratorTestSpec.groovy
+++ src/test/groovy/cloud/jnorthr/tools/configuration/ConfiguratorTestSpec.groovy
@@ -4,24 +4,34 @@ package cloud.jnorthr.tools.configuration;
 * see: http://code.google.com/p/spock/wiki/SpockBasics
 * A spock test wrapper around the base class
 */
+import cloud.jnorthr.tools.configuration.Configurator;
 //import CacheEntry;
 //import groovy.transform.Canonical
 //import groovy.transform.ToString
 import java.util.logging.Logger;
 import spock.lang.*
-import javax.swing.JFileChooser;
+//import javax.swing.JFileChooser;
 
 class ConfigutationTestSpec extends Specification 
 {
   // fields
   boolean flag = false;
-
+  Configurator ch;
+  	    
   //static Logger log = Logger.getLogger(CacheManagerTestSpock.class.getName());
 
-  // fixture methods
+  // Fixture Methods
+  
+  // run before every feature method
+  def setup() 
+  { 
+  	  ch = new Configurator(); 
+  }          
+
+  // run after every feature method
+  def cleanup() {}
+  
   // Note: The setupSpec() and cleanupSpec() methods may not reference instance fields.
-  def setup() {}          // run before every feature method
-  def cleanup() {}        // run after every feature method
   def setupSpec() {}     // run before the first feature method
   def cleanupSpec() {}   // run after the last feature method}
 
@@ -30,77 +40,200 @@ Feature methods are the heart of a specification. They describe the features (pr
 
 Conceptually, a feature method consists of four phases:
 
-Set up the feature's fixture
-Provide a stimulus to the system under specification
-Describe the response expected from the system
-Clean up the feature's fixture
-Whereas the first and last phases are optional, the stimulus and response phases are always present (except in interacting feature methods), and may occur more than once.
+ . Set up the feature's fixture
+ . Provide a stimulus to the system under specification
+ . Describe the response expected from the system
+ . Clean up the feature's fixture
+ . Whereas the first and last phases are optional, the stimulus and response phases are always present (except in interacting feature methods), and may occur more than once.
 
 */
 
   // Feature Methods
 
   // First Test
-  def "Setup Configurator for default path"() {
-    def ch;
+  def "1st Test: Setup Configurator for default path"() {
     given:
-    	// [] is Groovy literal for List and is infered
-        ch = new Configurator(); 
  
     when:
-	flag = ch.runner('prod');
+		flag = runner('prod');
  
     then:
-	// Asserts are implicit and not need to be stated.
+		// Asserts are implicit and not need to be stated.
     	// Change "==" to "!=" and see what's happening!
     	flag == true;
-  }
+  } // end of test
+
 
   // 2nd Test
   def "2nd Test: Set Configurator initial folder to unknown path"() {
     given:
-        def ch = new Configurator();
  
     when:
-	ch = new Configurator("/Fred");
+		ch = new Configurator("/Fred");
  
     then:
     	// Asserts are implicit and not need to be stated.
     	// Change "==" to "!=" and see what's happening!
     	def e = thrown(java.io.FileNotFoundException)
-	e.cause == null
+	    e.cause == null
   }
 
   // 3rd Test
   def "3rd Test: Ask appName for 'prod' environment"() {
     given:
-        def ch = new Configurator();
- 
+        
     when:
-		boolean  yn = ch.runner('prod')
+		boolean  yn = runner('prod')
  
     then:
     	// Asserts are implicit and not need to be stated.
     	// Change "==" to "!=" and see what's happening!
     	yn == true;
-		ch.getVal('appName') == 'production'
-  }
+		ch.get('appName') == 'production'
+  } // end of test
   
 
   // 4th Test
   def "4th Test: Ask app for 'local' mail.host"() {
     given:
-        def ch = new Configurator();
- 
+        
     when:
-		boolean  yn = ch.tester1('local')
+		boolean  yn = ch.setup('local')
     then:
     	// Asserts are implicit and not need to be stated.
     	// Change "==" to "!=" and see what's happening!
     	yn == true;
- 		ch.getVal('email') == 'james.northrop@orange.fr'
-  }
+ 		ch.get('email') == 'james.northrop@orange.fr'
+  } // end of text
   
-    // helper methods
+  
+  // Fifth Test
+  def "5th Test: Setup Configurator to use 'prod' environment"() {
+    given:
+ 
+    when:
+		flag = runner('prod');
+ 
+    then:
+		// Asserts are implicit and not need to be stated.
+    	flag == true;
+		println "getting email="+ch.get('email');
+		ch.get('email') == "james.b.northrop@googlemail.com"
+  } // end of test
+
+    
+  // Sixth Test
+  def "6th Test: Setup Configurator to use 'local' environment"() {
+    given:
+ 		
+    when:
+		flag = ch.setup('local');
+ 
+    then:
+		// Asserts are implicit and not need to be stated.
+    	flag == true;
+		println "getting email="+ch.get('email');
+		ch.get('email') == "james.northrop@orange.fr"
+  } // end of test
+
+
+  // Seventh Test
+  def "7th Test: Confirm dump() of configObject yields correct values"() {
+    given:
+		ch.dump() == "configObject=[data:[domain:cloud.jnorthr.tools, name:jnorthr], mail:[host:mail.server], email:james.b.northrop@googlemail.com, appName:production]";
+ 		
+    when:
+		def devMap = ['name':'Roberto', 'framework':'Grails', 'language':'Groovy']
+		ch.overwrite(devMap); 
+ 
+    then:
+		println ch.dump();
+		ch.dump() == "configObject=[data:[domain:cloud.jnorthr.tools, name:jnorthr], mail:[host:mail.server], email:james.b.northrop@googlemail.com, appName:production, name:Roberto, framework:Grails, language:Groovy]"
+  } // end of test
+
+
+  // Eighth Test
+  def "8th Test: Put a new 'alias' key entry into the config"() {
+  		println "8th Test: Put a new 'alias' key entry into the config"
+  		
+    given:
+		println ch.dump();
+		ch.dump() == "configObject=[data:[domain:cloud.jnorthr.tools, name:jnorthr], mail:[host:mail.server], email:james.b.northrop@googlemail.com, appName:production]" 		
+    when:
+		ch.put('alias','jnorthr');
+		 
+    then:
+		println ch.dump();
+		ch.dump() == "configObject=[data:[domain:cloud.jnorthr.tools, name:jnorthr], mail:[host:mail.server], email:james.b.northrop@googlemail.com, appName:production, alias:jnorthr]"
+  } // end of test
+
+
+
+  // Nineth Test
+  def "9th Test: Put a new 'alias' key entry into the config then check HAS()"() {
+  		println "9th Test: Put a new 'alias' key entry into the config then check HAS()"
+  		
+    given:
+		println ch.dump();
+		ch.dump() == "configObject=[data:[domain:cloud.jnorthr.tools, name:jnorthr], mail:[host:mail.server], email:james.b.northrop@googlemail.com, appName:production]" 		
+    when:
+		ch.put('alias','jnorthr');
+		 
+    then:
+		ch.has('alias') == true
+		ch.get('alias') == 'jnorthr';		
+  } // end of test
+
+
+  
+  	// =============================================================
+    // Helper Methods
+	def say(txt) { println txt; }
+
+	// Holds text found in the external configuration file
+	String configText="""// Custom block with setting
+// conditional per environment.
+servers {
+    local {
+        mail.host = 'greenmail'
+        email = 'james.northrop@orange.fr'
+    }
+
+    prod {
+        mail.host = 'mail.server'
+        email='james.b.northrop@googlemail.com'
+    }
+}
+
+environments {
+    local {
+        appName = 'local'
+    }
+    prod {
+        appName = 'production'
+    }
+}
+
+data{
+	domain='cloud.jnorthr.tools'
+	name='jnorthr'
+}
+""";
+
+	// construct runtime environment
+	public boolean runner(String e)
+	{
+		say "runner environment="+e;
+		
+		// Create Configurator slurper and set environment to prod.
+		def configObject = ch.createConfig(e).parse(configText)
+
+		say "prod Configurator.mail.host="+configObject.mail.host
+		assert configObject.mail.host == 'mail.server'
+
+		say "prod configObject.appName="+configObject.appName
+		configObject.appName == 'production';
+	} // end of runner
+
 } 
 
